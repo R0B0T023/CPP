cc69d09beb424c3e48b1d9a9a12d8554de7d3e36
Angel Cedillo <80221251+R0B0T023@users.noreply.github.com>
2021-03-31 21:28:31 -0600

    Add files via upload

---
+++ C++/SUMA.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, suma;
+    cout<<"Dame un numero: ";
+    cin>>num1;
+    
+    cout<<"Dame otro numero: ";
+    cin>>num2;
+    
+    suma=num1+num2;
+    cout<<num1<<" + "<<num2<<" = "<<suma<<"\n";
+    
+    return 0;
+}
