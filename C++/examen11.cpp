be34852594ccb7e44826e1c26e4964e9fa0e9fce
Angel Cedillo <80221251+R0B0T023@users.noreply.github.com>
2021-05-04 00:51:25 -0500

    Add files via upload

---
+++ C++/examen11.cpp
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int i=0;
+    char producto[5];
+    int precio[i];
+
+    for(i=1;i<5;i++)
+    {
+        cout<<"Dame el nombre del producto 1: ";
+        cin>>producto[i];
+        cout<<"Dame el precio del producto 1: ";
+        cin>>precio[i];
+        cout<<"Dame el nombre del producto 2: ";
+        cin>>producto[i];
+        cout<<"Dame el precio del producto 2: ";
+        cin>>precio[i];
+        cout<<"Dame el nombre del producto 3: ";
+        cin>>producto[i];
+        cout<<"Dame el precio del producto 3: ";
+        cin>>precio[i];
+        cout<<"Dame el nombre del producto 4: ";
+        cin>>producto[i];
+        cout<<"Dame el precio del producto 4: ";
+        cin>>precio[i];
+        cout<<"Dame el nombre del producto 5: ";
+        cin>>producto[i];
+        cout<<"Dame el precio del producto 5: ";
+        cin>>precio[i];
+    }
+    
+    return 0;
+}
